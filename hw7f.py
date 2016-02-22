0c2f5b1ac93b267a7dd344f0b0e586b1c2e02efe
cheecheelin <cheecheelin@cheecheelins-MacBook-Pro.local>
2016-02-22 11:07:50 -0800

    corrections

--- hw7f.py
+++ hw7f.py
@@ -2,23 +2,29 @@ import numpy as np
 from scipy.io.wavfile import read, write
 import matplotlib.pyplot as plt
 from scipy import fftpack 
-from scipy.fftpack import fft
+from scipy.fftpack import fft, ifft
 
 #read audio samples
 # sig= read("wooguy.wav")
-fs, data = read('wooguy.wav') # load the data
-print data 
-
+rate, data = read('wooguy.wav') # load the data
+# print data 
 a = data.T[0] # get the first track of 2 channel soundtrack
-
+# b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
 c = fft(a) # calculate fourier transform (complex numbers list)
 d = len(c)/2  # half of the fft list (real signal symmetry)
 
-c[np.abs(c) > 500] = 0
-main_sig=fftpack.ifft(c)
+print c 
+
+c[9000:9300] = 0
+c[-9300:-9000] = 0
+
+
+e= ifft(c)
+write('test.wav',rate,e.astype(data.dtype))
+
 
 # plt.plot(abs(c[:(d-1)]),'r') 
-plt.plot(abs(c[:d]),'r')
+plt.plot(abs(c[:(d-1)]),'r')
 plt.show()
 
 
@@ -26,20 +32,6 @@ plt.show()
 
 
 
-# audio= sig[1]
-
-# #plot the first 1000 samples
-# plt.plot(audio[0:1000])
-
-# #label axes 
-# plt.ylabel("amplitude")
-# plt.xlabel("frequency")
-
-# plt.title("wooguy")
-
-# plt.show()
 
 
-
-
-# b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
+# 
